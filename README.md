b04ae1015b9402085a195f7ca7a8ca94b90ecdc4
Ye Yuan <yeyu@umich.edu>
2025-02-08 10:30:49 -0500

    Siliconflow/Volcengine tested

--- README.md
+++ README.md
@@ -1,19 +1,37 @@
 # About
-OpenWebUI has recently added support for [thinking tags](https://github.com/open-webui/open-webui/blob/b72150c881955721a63ae7f4ea1b9ea293816fc1/CHANGELOG.md?plain=1#L39). However, reasoning tokens from reasoning models in OpenRouter, such as DeepSeek R1 and its distilled variants, are still not visible in OpenWebUI. This is because the OpenRouter API places them in a [separate "reasoning" field](https://openrouter.ai/announcements/reasoning-tokens-for-thinking-models) instead of including them in the actual response message.
+
+OpenWebUI has recently added support for [thinking tags](https://github.com/open-webui/open-webui/blob/b72150c881955721a63ae7f4ea1b9ea293816fc1/CHANGELOG.md?plain=1#L39). While the original DeepSeek API places reasoning tokens in the response message, third-party providers tend to place reasoning content in distinct fields that are not well-defined.
 
 This pipe enables the reasoning tokens of specified models and moves those tokens into the response message, allowing OpenWebUI to detect and display them visually.
 
+Forked from [repository](https://github.com/rmarfil3/openwebui-openrouter-reasoning-tokens) with modifications to support Siliconflow/Volcengine/etc.
+
+此pipeline支持第三方API推理过程输出。
+
 # Demo
-![Demo](https://github.com/rmarfil3/openwebui-openrouter-reasoning-tokens/blob/main/DEMO.gif)
 
-# Additional Notes
-After installing and enabling this pipe, new models will be added with the *Thinking...* feature. These models are prefixed with `reasoning/`:
-- reasoning/deepseek/deepseek-r1:free
-- reasoning/deepseek/deepseek-r1-distill-llama-70b
-- reasoning/deepseek/deepseek-r1
+![Demo](https://github.com/yeyuan98/openwebui-reasoning-tokens/blob/main/DEMO.gif)
 
-You can modify this list in the code under the `pipes()` function.
+# Supported providers
 
-You may encounter an issue with generated titles. To resolve this:
+OpenRouter
+: The OpenRouter API places reasoning in a ["reasoning" field](https://openrouter.ai/announcements/reasoning-tokens-for-thinking-models). You need to set `include_reasoning` for the `Api Reasoning Token Request` field in the pipeline configuration (see instruction below).
+
+Siliconflow/硅基流动
+: The Siliconflow API places reasoning in a ["reasoning_content" field](https://docs.siliconflow.cn/api-reference/chat-completions/chat-completions).
+
+Volcengine/火山引擎
+: The Volcengine API places reasoning in a ["reasoning_content" field](https://www.volcengine.com/docs/82379/1399009).Refer to this [guide](https://juejin.cn/post/7468328650338779176) to setup API endpoint.
+
+# Instruction
+
+After installing this pipe, configure it to provide API address, key, and model names. After enabling this pipe, new models will be added with the *Thinking...* feature. These models are prefixed with `reasoning/`.
+
+Open WebUI uses LLM for chatting as well as other tasks including title generation. Models added by this pipeline are not compatible with title generation. To resolve this issue:
+
 1. Navigate to the Admin Panel → Settings → Interface.
 2. Change the **Task Model** option to something other than "Current Model".
+
+# Limitations
+
+Usage statistics could not be displayed in OWUI even if the API returns usage information. This might get fixed as time permits.
--- reasoning_tokens_pipe.py
+++ reasoning_tokens_pipe.py
@@ -1,21 +1,20 @@
 """
-title: OpenRouter Reasoning Tokens
-author: rmarfil3
-repo: https://github.com/rmarfil3/openwebui-openrouter-reasoning-tokens
-date: 2025-01-30
+title: Reasoning Tokens
+author: yeyuan98
+repo: https://github.com/yeyuan98/openwebui-reasoning-tokens
+date: 2025-02-07
 version: 0.1
 license: MIT
 description: Enables reasoning tokens and displays them visually in OWUI
 
+Forked from @rmarfil3 (repo: https://github.com/rmarfil3/openwebui-openrouter-reasoning-tokens).
 
-After installing and enabling, new models will be added which will have the "Thinking..." feature.
-These models will be prefixed with "reasoning/".
+Refer to Github repo for instructions.
 
-You will probably have an issue with the generated titles.
-You can go to Admin Panel -> Settings -> Interface, then change the Task Model to something else instead of "Current Model".
+Current limitation: token count statistics does not display in OWUI
 """
-
 from pydantic import BaseModel, Field
+import logging
 import requests
 import json
 import time
@@ -23,21 +22,37 @@ import time
 
 class Pipe:
     class Valves(BaseModel):
-        OPENROUTER_API_BASE_URL: str = Field(default="https://openrouter.ai/api/v1")
-        OPENROUTER_API_KEY: str = Field(
+        API_BASE_URL: str = Field(default="https://api.siliconflow.cn/v1")
+        #API_BASE_URL: str = Field(default="https://ark.cn-beijing.volces.com/api/v3")
+        API_KEY: str = Field(
             default="",
-            description="Your OpenRouter API key for authentication",
+            description="API key for authentication",
         )
+        API_REASONING_TOKEN_REQUEST: str = Field(
+            default="",
+            description="Additional request field for reasoning tokens (use default if not needed)"
+        )
+        API_REASONING_TOKEN_FIELD: str = Field(
+            default="reasoning_content",
+            description="Field containing reasoning tokens in the response (vary by provider)"
+        )
+        API_MODELS: str = Field(
+            default="deepseek-ai/DeepSeek-R1",
+            description="Model names separated by comma without any space"
+        )
+        LOG_CONSOLE: bool = False
 
     def __init__(self):
         self.valves = self.Valves()
+        self.logger = logging.getLogger(__name__)
+    
+    def log(self, log_line):
+        if self.valves.LOG_CONSOLE:
+            self.logger.warning(log_line) # Somehow WARN is the threshold, not INFO
 
     def pipes(self):
-        models = [
-            'deepseek/deepseek-r1:free',
-            'deepseek/deepseek-r1-distill-llama-70b',
-            'deepseek/deepseek-r1',
-        ]
+        models = self.valves.API_MODELS.split(",")
+        self.log("Registered models: " + json.dumps(models))
 
         return [{
             "id": f"reasoning/{model}",
@@ -46,7 +61,7 @@ class Pipe:
 
     def pipe(self, body: dict):
         headers = {
-            "Authorization": f"Bearer {self.valves.OPENROUTER_API_KEY}",
+            "Authorization": f"Bearer {self.valves.API_KEY}",
             "Content-Type": "application/json",
             "HTTP-Referer": "https://openwebui.com/",
             "X-Title": "Open WebUI",
@@ -60,11 +75,16 @@ class Pipe:
                 modified_body["model"].split(".", 1)[-1].replace("reasoning/", "", 1)
             )
 
-        modified_body["include_reasoning"] = True
+        if self.valves.API_REASONING_TOKEN_REQUEST != "":
+            modified_body[self.valves.API_REASONING_TOKEN_REQUEST] = True
+        
+        self.log("Request POST:")
+        self.log("    Header: " + json.dumps(headers))
+        self.log("    Body: " + json.dumps(modified_body))
 
         try:
             response = requests.post(
-                f"{self.valves.OPENROUTER_API_BASE_URL}/chat/completions",
+                f"{self.valves.API_BASE_URL}/chat/completions",
                 json=modified_body,
                 headers=headers,
                 stream=body.get("stream", False),
@@ -76,21 +96,26 @@ class Pipe:
             else:
                 return self._handle_normal_response(response, modified_body)
         except Exception as e:
-            return f"Error: {e}, {modified_body}"
+            return f"Error: {e}, Body: {modified_body}"
 
     def _handle_normal_response(self, response, body: dict):
         data = response.json()
+        self.log("Response [Normal]: " + data)
+        reasoning_field = self.valves.API_REASONING_TOKEN_FIELD
         if "choices" in data:
             for choice in data["choices"]:
-                if "message" in choice and "reasoning" in choice["message"]:
-                    reasoning = choice["message"]["reasoning"]
+                if "message" in choice and reasoning_field in choice["message"]:
+                    reasoning = choice["message"][reasoning_field]
+                    content = choice["message"]["content"]
                     choice["message"][
                         "content"
-                    ] = f"<think>{reasoning}</think>\n{choice['message']['content']}"
+                    ] = f"<think>{reasoning}</think>\n{content}"
         return data
 
     def _handle_streaming_response(self, response, body: dict):
         thinking_state = -1  # -1: not started, 0: thinking, 1: answered
+        reasoning_field = self.valves.API_REASONING_TOKEN_FIELD
+        self.log("Response [Streaming, lines below]:")
 
         def construct_chunk(content, role='assistant'):
             return f"""data: {json.dumps({
@@ -102,15 +127,19 @@ class Pipe:
                     'index': 0,
                     'delta': {'content': content, 'role': role},
                     'finish_reason': None
-                }]
+                }],
+                'info': data.get('usage', {})
             })}\n\n"""
 
         for line in response.iter_lines():
             if not line:
                 continue
             decoded_line = line.decode("utf-8")
+            self.log("    Line: " + decoded_line)
             try:
                 if not decoded_line.startswith("data: "):
+                    continue
+                if "data: [DONE]" in decoded_line:
                     continue
                 data = json.loads(decoded_line[6:])  # Skip "data: " prefix
                 if not data.get("choices"):
@@ -119,15 +148,17 @@ class Pipe:
                 delta = choice.get("delta", {})
 
                 # Handle transitioning states
-                if thinking_state == -1 and delta.get("reasoning"):
+                if thinking_state == -1 and delta.get(reasoning_field):
+                    self.log("    Transition: -1 -> 0" + decoded_line)
                     thinking_state = 0
                     yield construct_chunk("<think>")
-                elif thinking_state == 0 and not delta.get("reasoning"):
+                elif thinking_state == 0 and not delta.get(reasoning_field):
+                    self.log("    Transition: 0 -> 1" + decoded_line)
                     thinking_state = 1
                     yield construct_chunk("</think>\n\n")
 
                 # Handle content output
-                content = delta.get("reasoning", "") or delta.get("content", "")
+                content = delta.get(reasoning_field, "") or delta.get("content", "")
                 if content:
                     yield construct_chunk(content)
             except json.JSONDecodeError:
